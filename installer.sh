23af11eb1f7f2e2e9da1ad9f0f355f90ab0cb651
madiunprogramer <69541278+madiunprogramer@users.noreply.github.com>
2020-08-15 00:33:39 +0700

    Up

--- installer.sh
+++ installer.sh
@@ -1,20 +1,47 @@
-#!/bin/bash
+#!/bin/bash env
+
+R='\033[1;31m'
+G='\033[1;32m'
+Y='\033[1;33m'
+N='\033[0m'
+clear
+
+ABI=$(getprop ro.product.cpu.abi)
+ARCH="${ABI%-*}"
+sleep 3& PID=$! #simulate a long process
+
+echo "${R}Proses cek architecture...${N}"
+printf "[ "
+# While process is running...
+while kill -0 $PID 2> /dev/null; do 
+    printf  "#"
+    sleep .1
+done
+printf " ]"
+echo -n "
+
+
+
+
+
+
+"
+echo "System archi terdeteksi = ${R}${ARCH}${N}"
+sleep 5
+
 clear
 
 cd ~
 
-RED='\033[0;31m'
-Green='\033[0;32m'
-NC='\033[0m' # No Color
+lxdf="${G}Running Auto installer....${N}"
 
-lxdf="Running Auto installer....\nby : t.me/AnonimHelper"
 echo $lxdf
 pkg uninstall golang -y > /dev/null 2>&1
 clear
 echo $lxdf
 
 echo -n "
-${Gren}Step 1 : Update & Upgrade repo.${NC}
+${Y}Step 1 : Update & Upgrade repo.${N}
 
 "
 apt update && apt upgrade
@@ -22,7 +49,7 @@ sleep 3
 clear
 echo $lxdf
 echo -n "
-Step 2 : Installing git & golang..
+${Y}Step 2 : Installing git & golang..${N}
 
 "
 
@@ -31,22 +58,31 @@ sleep 3
 clear 
 echo $lxdf
 echo -n "
-Step 3 : Installing golang...
+${Y}Step 3 : Installing golang...${N}
 
 "
 cd
 mkdir -p foo
 cd foo
+
+if [ $ARCH = 'arm' ]
+then
+wget -N --progress=bar:force https://termux.mentality.rip/termux-packages-24/arm/golang_2%3A1.14.7-1_arm.deb 2>&1 
+dpkg -i golang_2:1.14.7-1_arm.deb
+fi
+if [ $ARCH = 'arm64' ]
+then
 wget -N --progress=bar:force  https://termux.mentality.rip/termux-packages-24/aarch64/golang_2%3A1.14.7-1_aarch64.deb 2>&1 
 dpkg -i golang_2:1.14.7-1_aarch64.deb
+fi
+
 cd 
 sleep 3
 clear 
 echo $lxdf
 
 echo -n "
-
-Step 4 : Installing popon-skuy...
+${Y}Step 4 : Installing popon-skuy...${N}
 
 "
 go get -v -u -d github.com/madiunprogramer/popon-skuy
@@ -59,7 +95,7 @@ sleep 3
 clear 
 echo $lxdf
 echo -n "
-Step 4 : Installing Tunnel Core...
+${Y}Step 4 : Installing Tunnel Core...${N}
 
 "
 go get -v -u -d github.com/Psiphon-Labs/psiphon-tunnel-core/ConsoleClient
@@ -71,10 +107,5 @@ sleep 2
 
 cd ~/go/src/github.com/madiunprogramer/popon-skuy
 ./popon-skuy
-
-
-
-
-
 
 
